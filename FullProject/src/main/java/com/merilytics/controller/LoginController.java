10a683406de987e162a790311280114fb2902e11
sameer <sameer@Lenovo-pc>
2017-07-16 22:34:40 +0530

    unable to add new classes

--- FullProject/src/main/java/com/merilytics/controller/LoginController.java
+++ FullProject/src/main/java/com/merilytics/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.merilytics.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.merilytics.dto.UsersDTO;
+import com.merilytics.service.UsersService;
 
 /**
  * @author sameer
@@ -17,22 +21,30 @@ import com.merilytics.dto.UsersDTO;
  */
 @Controller
 public class LoginController {
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public @ResponseBody String loginPage(@RequestBody UsersDTO map){
-		System.out.println("inside login controller");
-		System.out.println(map.getuName()+"        "+map.getPwd());
-		//System.out.println(userName.length());
-		//System.out.println(userName+""+passWord);
+@Inject
+	private UsersService service;
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public @ResponseBody String loginPage(@RequestBody UsersDTO map) {
+	/*	System.out.println("inside login controller");
+		System.out.println(map.getuName() + "        " + map.getPwd());
+		// System.out.println(userName.length());
+		// System.out.println(userName+""+passWord);
+*/	
+		 UsersDTO dt=	service.loginService(map);
+		 if(dt!=null)
 		return "logged in successfully";
+		 else
+			 return "login failure";
 	}
-	@RequestMapping(value="/test",method=RequestMethod.GET)
-	public @ResponseBody List test(){
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public @ResponseBody List test() {
 		System.out.println("inside test suresh controller");
-		List map= new ArrayList();
+		List map = new ArrayList();
 		map.add("edfcevw");
 		System.out.println("changes");
-		//what the fuck is this
+		// what the fuck is this
 		return map;
 	}
-//first github project
+	// first github project
 }
--- FullProject/src/main/java/com/merilytics/dto/UsersDTO.java
+++ FullProject/src/main/java/com/merilytics/dto/UsersDTO.java
@@ -3,11 +3,20 @@ package com.merilytics.dto;
 import java.io.Serializable;
 
 public class UsersDTO implements Serializable {
+	private int userID;
 	private String uName;
 	private String pwd;
 
 	public String getuName() {
 		return uName;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
 	}
 
 	public void setuName(String uName) {
