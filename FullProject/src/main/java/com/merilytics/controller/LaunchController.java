20b950cef6ea502638ba1215035de8131b75f7fa
sameer <sameer@Lenovo-pc>
2017-07-16 16:22:02 +0530

    new commit

--- FullProject/src/main/java/com/merilytics/controller/LaunchController.java
+++ FullProject/src/main/java/com/merilytics/controller/LaunchController.java
@@ -3,7 +3,7 @@ package com.merilytics.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//new changes
 @Controller
 public class LaunchController {
 	@RequestMapping(value="/getting.ht",method=RequestMethod.GET)
