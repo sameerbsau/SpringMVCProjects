ed62c70d5bd6be358e757f88cfd8827eaaee7bb2
sameerbsau <sameerbsau@users.noreply.github.com>
2017-08-10 17:53:20 +0530

    from workstation

--- FullProject/src/main/java/com/merilytics/bo/Test.java
+++ FullProject/src/main/java/com/merilytics/bo/Test.java
@@ -1,5 +1,12 @@
 package com.merilytics.bo;
 
+/**
+ * @project FullProject
+ * @author 21258
+ * @timeAndYear 5:52:07 PM 10-Aug-2017
+ * @description 
+ * 
+ */
 public class Test {
 //test bi eiahdahdkh
 }
